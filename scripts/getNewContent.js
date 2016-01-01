8339ec8c4881ae7625ae681d2ea6fa4f75c798d5
xieminjie <xieminjiexmj@gmail.com>
2016-01-01 11:38:09 +1100

    update

---
+++ scripts/getNewContent.js
@@ -0,0 +1,17 @@
+function getNewContent(){
+	var request = getHTTPObject();
+	if (request) {
+		request.open("GET","response.txt",true);
+		request.onreadystatechange = function(){
+			if (request.readyState==4) {
+				var para = document.createElement("p");
+				var txt = document.createTextNode(request.responseText);
+				para.appendChild(txt);
+				document.getElementById("new").appendChild(para);
+			};
+			request.send(null);
+		}
+	}else{
+		alert("sorry");
+	}
+}
