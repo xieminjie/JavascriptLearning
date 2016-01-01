6e7469724489fe7d57454ed7598fad331ff73632
xieminjie <xieminjiexmj@gmail.com>
2016-01-01 16:14:12 +1100

    update

--- scripts/menuAnimation.js
+++ scripts/menuAnimation.js
@@ -1,11 +1,8 @@
 $(document).ready(function(){
 	$('.menu_btn').click(function(){
+		$('.menu_btn').hide();
 		$('.menu').animate({
       	left: "0px"
-   		 }, 200)
-
-    	$('main').animate({
-      	left: "285px"
-    	}, 200);
+   		 }, 200);
 	});
 });
