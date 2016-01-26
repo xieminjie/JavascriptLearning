d0a81102b88a3c3fd74913e7a175f794f73b2030
xieminjie <xieminjiexmj@gmail.com>
2016-01-26 13:55:02 +1100

    updated

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,18 @@
-var app = angular.module("store",[]);
+(function(){
+	var app = angular.module("store",[]);
+	app.controller('StoreController',function(){
+		this.products = gems;
+	});
+	var gems = [
+		{
+			name:'Dodecahedron',
+			price:2.95,
+			description:"This is a good one"
+		},
+		{
+			name:'Test',
+			price:1.95,
+			description:"This is nice"
+		}
+	];
+})();
