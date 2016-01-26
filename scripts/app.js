a6b87c6d3aaea90443a01b44b5fe44309f0bf1fe
xieminjie <xieminjiexmj@gmail.com>
2016-01-26 19:54:38 +1100

    updated

--- scripts/app.js
+++ scripts/app.js
@@ -3,16 +3,47 @@
 	app.controller('StoreController',function(){
 		this.products = gems;
 	});
+	app.controller('PanelController',function(){
+		this.tab = 1;
+		this.selectTab = function(setTab){
+			this.tab = setTab; 
+		};
+		this.isSelected = function(checkTab){
+			return this.tab  === checkTab;
+		};
+	});
 	var gems = [
 		{
 			name:'Dodecahedron',
 			price:2.95,
-			description:"This is a good one"
+			description:"This is a good one",
+			reviews:[{
+				stars:5,
+				body:"I love this product",
+				author:"joe@thomas.com"
+				},
+				{
+				stars:1,
+				body:"I hate this product",
+				author:"e@thomas.com"
+				}
+			]
 		},
 		{
-			name:'Test',
-			price:1.95,
-			description:"This is nice"
+			name:'Dodecahedron',
+			price:2.95,
+			description:"This is a good one",
+			reviews:[{
+				stars:5,
+				body:"I love this product",
+				author:"joe@thomas.com"
+				},
+				{
+				stars:1,
+				body:"I hate this product",
+				author:"e@thomas.com"
+				}
+			]
 		}
 	];
 })();
